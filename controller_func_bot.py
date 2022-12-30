d1a6d8818a577e45f44eab44772b07271674b447
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-12-30 15:00:54 +0300

    add setting translate bot

--- controller_func_bot.py
+++ controller_func_bot.py
@@ -1,16 +1,33 @@
 from aiogram import types
-
+from translate import Translator
 from bot_config import *
 import func_translator_text as ftt
+import aiogram
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm TranslatorBot!\nPowered by aiogram.\nYou send me text for translate")
+    print(message)
+    await message.reply(f"Hi, {message.from_user.first_name}!\nI'm TranslatorBot!\nYou send me choised mode translate:\n /rusEn - с русского на аглийский\n /enRus - c английского на русcкий")
+
+
+@dp.message_handler(commands=['rusEn', 'enRus'])
+async def choise_mode(message: types.Message):
+
+    if message.text == '/rusEn':
+        ftt.translator = Translator(from_lang='ru', to_lang='en')
+    else:
+        ftt.translator = Translator(from_lang='en', to_lang='ru')
+    await message.reply('setting is save.\n If you want to change the settings:\n /rusEn - с русского на аглийский\n /enRus - c английского на русcкий')
 
 
 @dp.message_handler()
 async def translate_echo(message: types.Message):
     translated_message = message.text + \
-        ' перевод готов: ' + ftt.translate(message.text)
+        ' - переводится как: ' + ftt.translate(message.text)
     await message.answer(translated_message)
+
+
+@dp.message_handler(content_types='voice')
+async def voice_echo(message: types.Message):
+    await bot.send_voice(message.voice)
--- func_translator_text.py
+++ func_translator_text.py
@@ -1,7 +1,30 @@
+import speech_recognition as sr
 from translate import Translator
+import pyttsx3
 
-translator = Translator(from_lang='ru', to_lang='en')
+vioce_engine = pyttsx3.init()
+translator = Translator(from_lang='', to_lang='')
 
 
 def translate(_text):
     return translator.translate(_text)
+
+
+# def command():
+#     r = sr.Recognizer()
+
+#     with sr.Microphone() as sourse:
+#         print('Скажите что-то')
+#         r.pause_threshold = 1
+#         r.adjust_for_ambient_noise(sourse, duration=1)
+#         audio = r.listen(sourse)
+
+#     try:
+#         recognized_text = r.recognize_google(audio, language='en-EN')
+#         print('распознано: '+recognized_text)
+
+#     except sr.UnknownValueError:
+#         talk('повторите еще раз')
+#         recognazed_text = command()
+
+#         return recognized_text
